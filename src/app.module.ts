f459250bb0e88cc3f9778854bfa420043f86f998
phuchv <141104806+vfa-phuchv@users.noreply.github.com>
2023-08-03 09:53:24 +0700

    integrate and config graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {GraphQLModule} from '@nestjs/graphql'
+import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo'
+import {UserModule} from './modules/user/user.module'
 
 @Module({
-  imports: [],
+  imports: [
+    // Init the grapthQL module
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      typePaths: ['./**/*.graphql']
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
